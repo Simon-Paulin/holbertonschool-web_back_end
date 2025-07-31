42d12d7e1e55cc17a1185fab57ff63a0dacc68c8
Simon-Paulin <paulin.simon@live.fr>
2025-07-31 13:55:29 +0200

    task5

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+list of floats
+"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    return sum of values in a list
+    """
+    return sum(input_list)
