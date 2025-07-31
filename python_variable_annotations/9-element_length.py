658cdca6474a22005ac484b3441fc169fcaa45fb
Simon-Paulin <paulin.simon@live.fr>
2025-07-31 15:21:11 +0200

    task09

---
+++ python_variable_annotations/9-element_length.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+return value with type
+"""
+from typing import Iterable, Sequence, Tuple, List
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """
+    return value with type
+    """
+    return [(i, len(i)) for i in lst]
