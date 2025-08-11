21b17908d2945a0fd2dcd0b079570c2fb439b9c4
Simon-Paulin <paulin.simon@live.fr>
2025-08-11 11:06:57 +0200

    fix pyco

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-'''
-'''
+"""
+"""
 
 
 def index_range(page: int, page_size: int) -> tuple:
-    '''
-    '''
+    """
+    """
     st_index = (page - 1) * page_size
     end_index = page * page_size
     return (st_index, end_index)
