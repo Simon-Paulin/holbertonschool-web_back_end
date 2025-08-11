55a41f68a525110cefc4f8dced7c452956965600
Simon-Paulin <paulin.simon@live.fr>
2025-08-11 12:11:52 +0200

    fix

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -9,23 +9,21 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
     end_index = page + page_size
     return (st_index, end_index)
 
+
 class Server:
-    """Server class to paginate a database of popular baby names.
-    """
+    """Server class to paginate a database of popular baby names"""
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
         self.__dataset = None
 
     def dataset(self) -> List[List]:
-        """Cached dataset
-        """
+        """Cached dataset"""
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
                 dataset = [row for row in reader]
             self.__dataset = dataset[1:]
-
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
