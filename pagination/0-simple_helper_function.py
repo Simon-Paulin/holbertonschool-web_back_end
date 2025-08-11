5f705b2556a3ebb7001cc1315eea18a9d18b7ee7
Simon-Paulin <paulin.simon@live.fr>
2025-08-11 11:16:54 +0200

    fiiiiiix

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,13 +1,9 @@
 #!/usr/bin/env python3
-"""
-Simple helper function 
-"""
+"""Simple helper function """
 
 
 def index_range(page: int, page_size: int) -> tuple:
-    """
-    Simple helper function 
-    """
+    """Simple helper function """
     st_index = (page - 1) * page_size
     end_index = page * page_size
     return (st_index, end_index)
