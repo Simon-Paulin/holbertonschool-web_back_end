bd575fad4807e70f48b9f7044e21c46dd6bbe73a
Simon-Paulin <paulin.simon@live.fr>
2025-08-12 15:11:18 +0200

    fix import

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 """list all documents"""
+from pymongo import MongoClient
+
+client = MongoClient('mongodb://127.0.0.1:27017')
+mongo_collection = client.my_db.school
 
 
 def list_all(mongo_collection):
-    """"""
+    """list"""
     docs = list (mongo_collection.find())
     if len(docs) == 0:
         return[]
